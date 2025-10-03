1da8f7e052f962dccf9f0ba4d74ddd5517386c29
あ <k024g0101@g.neec.ac.jp>
2025-10-03 19:51:02 +0900

    一旦

--- project/Source/Engine/PSO.cpp
+++ project/Source/Engine/PSO.cpp
@@ -37,6 +37,30 @@ void PSO::Create(
         graphicsPipelineStateDesc[i].DSVFormat = DXGI_FORMAT_D24_UNORM_S8_UINT;
     }
 
+
+    for (int i = PARTICLE; i < PSO_MAX; ++i) {
+
+        graphicsPipelineStateDesc[i].pRootSignature = rootSignature.GetRootSignature(1).Get();//RootSignature
+        graphicsPipelineStateDesc[i].InputLayout = inputLayout.GetDesc();//InputLayout
+        graphicsPipelineStateDesc[i].VS = { dxcCompiler.GetVertexShaderBlob(1)->GetBufferPointer(),
+       dxcCompiler.GetVertexShaderBlob(1)->GetBufferSize() };//VertexShader
+        graphicsPipelineStateDesc[i].PS = { dxcCompiler.GetPixelShaderBlob(1)->GetBufferPointer(),
+       dxcCompiler.GetPixelShaderBlob(1)->GetBufferSize() };//PixelShader
+        graphicsPipelineStateDesc[i].BlendState = blendState.GetDesc();//BlendState
+        graphicsPipelineStateDesc[i].RasterizerState = rasterizerState.GetDesc();//RasterizerState
+        //書き込むRTVの情報
+        graphicsPipelineStateDesc[i].NumRenderTargets = 1;
+        graphicsPipelineStateDesc[i].RTVFormats[0] = DXGI_FORMAT_R8G8B8A8_UNORM_SRGB;
+
+        //どのように画面に色を打ち込むかの設定（気にしなくていい）
+        graphicsPipelineStateDesc[i].SampleDesc.Count = 1;
+        graphicsPipelineStateDesc[i].SampleMask = D3D12_DEFAULT_SAMPLE_MASK;
+        //DepthStencilの設定   
+        graphicsPipelineStateDesc[i].DepthStencilState = depthStencil.GetDesc();
+        graphicsPipelineStateDesc[i].DSVFormat = DXGI_FORMAT_D24_UNORM_S8_UINT;
+    }
+
+
     //利用するトポロジ（形状）のタイプ。
     graphicsPipelineStateDesc[TRIANGLE].PrimitiveTopologyType =
         D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
@@ -46,7 +70,8 @@ void PSO::Create(
 
     graphicsPipelineStateDesc[POINT].PrimitiveTopologyType =
         D3D12_PRIMITIVE_TOPOLOGY_TYPE_POINT;
-
+    graphicsPipelineStateDesc[PARTICLE].PrimitiveTopologyType =
+        D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
 
     for (int i = 0; i < PSO_MAX; ++i) {
 
