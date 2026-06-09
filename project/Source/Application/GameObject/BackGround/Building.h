17a022dce48949d9592208bc22ef5953f339fc03
めいそ <k024g0101@g.neec.ac.jp>
2026-06-09 11:27:04 +0900

    メモが床に隠れる不具合を解決

--- project/Source/Application/GameObject/BackGround/Building.h
+++ project/Source/Application/GameObject/BackGround/Building.h
@@ -53,7 +53,7 @@ private:
     const float kWallMaxHeight_ = 5.0f;
     const float kWallMinHeight_ = -1.0f;
     const float kWallThickness_ = 0.5f;
-    const float kFloorThickness_ = 0.5f;
+    const float kFloorThickness_ = 0.25f;
     //壁の座標を設定する
     const float kWallPosXZ_ = 26.0f;
     const float kWallOriginPos_ = 0.0f;
